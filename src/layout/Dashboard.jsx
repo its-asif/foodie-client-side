bc71e9a0517e08becca0b79c589a4fc4a61dd74c
its-asif <asifiscoding@gmail.com>
2024-03-11 03:45:37 +0600

    up

--- src/layout/Dashboard.jsx
+++ src/layout/Dashboard.jsx
@@ -17,17 +17,17 @@ const Dashboard = () => {
                             <li><NavLink to='/dashboard/adminHome'><FaHome></FaHome> Admin Home</NavLink></li>
                             <li><NavLink to='/dashboard/addItems'><FaUtensils></FaUtensils> Add Items</NavLink></li>
                             <li><NavLink to='/dashboard/manageItems'><FaList></FaList> Manage Items</NavLink></li>
-                            <li><NavLink to='/dashboard/bookings'><FaAd></FaAd> Manage Bookings</NavLink></li>
+                            {/* <li><NavLink to='/dashboard/bookings'><FaAd></FaAd> Manage Bookings</NavLink></li> */}
                             <li><NavLink to='/dashboard/users'><FaUsers></FaUsers> All Users</NavLink></li>
                         </>
                         :
                         <>
                             <li><NavLink to='/dashboard/userHome'><FaHome></FaHome> User Home</NavLink></li>
-                            <li><NavLink to='/dashboard/reservation'><FaCalculator></FaCalculator> Reservation</NavLink></li>
+                            {/* <li><NavLink to='/dashboard/reservation'><FaCalculator></FaCalculator> Reservation</NavLink></li> */}
                             <li><NavLink to='/dashboard/paymentHistory'><FaWallet></FaWallet> Payment History</NavLink></li>
                             <li><NavLink to='/dashboard/cart'><FaShoppingCart></FaShoppingCart> My Cart</NavLink></li>
-                            <li><NavLink to='/dashboard/review'><FaStar></FaStar> My Reviews</NavLink></li>
-                            <li><NavLink to='/dashboard/bookings'><FaList></FaList> My Booking</NavLink></li>
+                            {/* <li><NavLink to='/dashboard/review'><FaStar></FaStar> My Reviews</NavLink></li> */}
+                            {/* <li><NavLink to='/dashboard/bookings'><FaList></FaList> My Booking</NavLink></li> */}
                         </>
                     }
 
@@ -35,7 +35,7 @@ const Dashboard = () => {
 
                     <li><NavLink to='/'><FaHome></FaHome> Home </NavLink></li>
                     <li><NavLink to='/menu'><FaSearch></FaSearch> Menu </NavLink></li>
-                    <li><NavLink to='/order/contact'><FaEnvelope></FaEnvelope> Contact </NavLink></li>
+                    {/* <li><NavLink to='/order/contact'><FaEnvelope></FaEnvelope> Contact </NavLink></li> */}
                     
                 </ul>
             </div>
